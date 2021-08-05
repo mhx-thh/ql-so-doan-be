78e5d792b7d14cba64b4befb547a45c84d34c686
Ngô Nhật Tân <ngotanvn2001@gmail.com>
2021-08-05 18:44:43 +0700

    chuyển html thành pdf và gửi file qua mail

--- controller/sendMail.js
+++ controller/sendMail.js
@@ -1,10 +1,13 @@
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
-var handlebars = require('handlebars');
-var fs = require('fs');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const pdf = require('html-pdf');
+const sendResponse = require('../utils/sendResponse');
+const AppError = require('../utils/appError');
 
 exports.sendMail = (req, res, next) => {
-  var readHTMLFile = function (path, callback) {
+  const readHTMLFile = function (path, callback) {
     fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
       if (err) {
         throw err;
@@ -16,7 +19,7 @@ exports.sendMail = (req, res, next) => {
     });
   };
 
-  var transporter = nodemailer.createTransport(smtpTransport({
+  const transporter = nodemailer.createTransport(smtpTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     auth: {
@@ -25,24 +28,36 @@ exports.sendMail = (req, res, next) => {
     }
   }));
 
+
   readHTMLFile(process.env.LINK_HTML, function (err, html) {
-    var template = handlebars.compile(html);
-    var replacements = req.body
-    var htmlToSend = template(replacements);
-    var mailOptions = {
-      from: process.env.GMAIL_EMAIL,
-      to: req.body.email,
-      subject: req.body.subject,
-      html: htmlToSend
+    const template = handlebars.compile(html);
+    const replacements = req.body
+    const html_replaced = template(replacements);
+    const pdf_options = {
+      format: 'A4',
+      orientation: 'portrait',
     };
-    transporter.sendMail(mailOptions, function (error, response) {
-      if (error) {
-        console.log(error);
-        return res.status(404).json({ "message": "Sent mail failed!" });
-      } else {
-        console.log('Email sent: ' + response.response);
-        return res.status(200).json({ "message": "Sent mail successfull!" });
-      }
+    pdf.create(html_replaced, pdf_options).toBuffer((err, buffer) => {
+      if (err) return next(AppError('Can not create PDF', 400, err));
+      const mailOptions = {
+        from: process.env.GMAIL_EMAIL,
+        to: req.body.email,
+        subject: req.body.subject,
+        text: req.body.text,
+        attachments: {
+          filename: process.env.FILE_NAME || 'bien_nhan.pdf',
+          content: new Buffer.from(buffer)
+        }
+      };
+      transporter.sendMail(mailOptions, function (error, response) {
+        if (error) {
+          console.log(error);
+          return res.status(404).json({ "message": "Sent mail failed!" });
+        } else {
+          console.log('Email sent: ' + response.response);
+          return res.status(200).json({ "message": "Sent mail successfull!" });
+        }
+      });
     });
-  })
-}
+  });
+};
