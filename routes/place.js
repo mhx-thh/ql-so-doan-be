1f06c1d32290e502a8f33a4cf2a23b789d5e6706
Ngô Nhật Tân <ngotanvn2001@gmail.com>
2021-08-09 13:24:04 +0700

    nothing change

--- routes/place.js
+++ routes/place.js
@@ -5,13 +5,13 @@ const PlaceController = require('../controller/place');
 
 //get all places 
 router.get('', PlaceController.getAll);
-//get place by id (req.body)
+//get place by id 
 router.get('/:id', PlaceController.getPlace);
 //add place
 router.post('/create', PlaceController.createPlace);
-//update place by id (req.body)
+//update place by id 
 router.put('/update/:id', PlaceController.updatePlace);
-//delete place by id (req.body)
+//delete place by id 
 router.delete('/delete/:id', PlaceController.deletePlace);
 
 module.exports = router;
